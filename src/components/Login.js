8753490853596ffd6b324ded0a3004023b9bcd22
tusharagrwall <tusharagrawalgeek@gmail.com>
2023-02-10 03:28:58 +0530

    1.1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,48 @@
-import { display } from "@mui/system";
-import React from "react";
+import React,{useState} from "react";
 import * as colors from "../colors.js";
 import '../style.css';
-function Login(){
-    function handleClick(){
-
+function Login(props){
+    const[state,setState]=useState(
+        {
+            username:"",
+            pswd:""
+        }
+    );
+    function handleChange(e){
+        var obj=e.target;
+        setState(p=>{
+            return({
+                ...p,
+                [obj.name]:obj.value
+            });
+        });
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(state.username,state.pswd);
     }
     return(
         <>
             <div className="container">
                 <div className="form">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <input type="text"
                          className="input"
-                         placeholder="Username">
+                         placeholder="Username"
+                         name="username"
+                         value={state.username}
+                         onChange={handleChange}>
                         </input><br/>
                         <input type="password" 
                          className="input"
-                         placeholder="Password">
+                         placeholder="Password"
+                         name="pswd"
+                         value={state.pswd}
+                         onChange={handleChange}>
                         </input><br/>
                         <button
                          className="btn-1" 
-                         type="submit"
-                         onClick={handleClick}>
+                         type="submit">
                             Login
                         </button>
                     </form>
