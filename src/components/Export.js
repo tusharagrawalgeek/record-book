e42a75a583d788443ac7c45c2a30ef2193c20017
tusharagrwall <tusharagrawalgeek@gmail.com>
2023-03-10 23:35:38 +0530

    export

--- src/components/Export.js
+++ src/components/Export.js
@@ -4,90 +4,290 @@ import '../style.css';
 import * as color from '../colors.js';
 import axios from "axios";
 import url from '../vars.js';
+import Loader from './Loader.js';
+import Modal from './Modal.js';
 function Export(){
     const [state,setState]=useState(
-        {
-            items:[],
-            showExportForm:false,
+        {   dateExported:new Date().getDate()+" / "+(new Date().getMonth()+1)+" / "+new Date().getFullYear(),
+            exportedItems:[],
+            showExportedItems:true,
+            showExportForm1:false,
+            showExportForm2:false,
             currentItems:[],
+            exportItemCount:1,
             particular:"",
-            quantity:"",
-            
+            quantity:[0],
+            id:[""],
+            exportedTo:"",
+            purpose:"",
+            showModal:false,
+            message:"",
+            messageVariant:"",
+            showLoader:false,
+            startExport:false
         }
     );
+    useEffect(()=>{
+        if(state.showModal){
+            setTimeout(()=>{
+                setState(p=>(
+                    {   
+                        ...p,
+                        showModal:false,
+                        
+                    }
+                ))
+            },3000)
+        }
+    },[state.showModal])
+    useEffect(()=>{
+        if(state.startExport){
+            const promises=[]
+            state.id.map((i,index)=>{
+                state.currentItems.some(j=>{
+                    if(j._id===i){
+                        var promise=exportItem(j,state.quantity[index]);
+                        promises.push(promise)
+                        return true;
+                    }
+                    return false;
+                })
+            })
+            Promise.all(promises)
+            .then((res)=>{
+                console.log(promises);
+                setState(p=>(
+                    {
+                        ...p,
+                        showLoader:false,
+                        message:"Exported successfully",
+                        messageVariant:"success",
+                        showModal:true,
+                        showExportForm1:true,
+                        showExportForm2:false,
+                        exportItemCount:1,
+                        particular:"",
+                        quantity:[0],
+                        id:[""],
+                        exportedTo:"",
+                        purpose:"",
+                        startExport:false
+                    }
+                ))
+            })
+            .catch(err=>{
+                setState(p=>(
+                    {
+                        ...p,
+                        showLoader:false,
+                        message:"Couldn't export",
+                        messageVariant:"error",
+                        showModal:true,
+                        startExport:false
+                    }
+                ))
+            })
+        }
+    },[state.startExport])
+    function handleQuantityChange(e){
+        const obj=e.target;
+        console.log(obj.name,obj.value);
+        state.quantity[[obj.name]]=obj.value
+        setState(p=>({...p}))
+    }
     function handleChange(e){
         const obj=e.target;
+        console.log(obj.name);
+        setState(p=>(
+            {
+                ...p,
+                [obj.name]:obj.value
+            }
+        ))
 
     }
-    function handleClick(){
-        axios.get(url+'/getitem')
+    function handleAddItem(){
+        state.quantity.push(0)
+        setState(p=>({
+            ...p,
+            exportItemCount:state.exportItemCount+1,
+            
+        }))
+    }
+    function handleClick(e){
+        const obj=e.target;
+        console.log(obj);
+        const name=obj.getAttribute("name");
+        var arr={showExportForm1:false,showExportForm2:false,showExportedItems:false}
+        if(name==="showExportForm2"){
+            axios.get(url+'/getitem')
         .then(res=>{
             const items=res.data.data;
             setState(p=>{
                 return({
                     ...p,
+                    ...arr,
+                    [obj.name]:!state[obj.name],
                     currentItems:items,
-                    itemToExport:null,
-                    showExportForm:!state.showExportForm
+                    // itemToExport:null,
                 });
             })
         })
         .catch(err=>console.log(err))
+           
+        }else{
+            setState(p=>(
+                {
+                    ...p,
+                    ...arr,
+                    [obj.name]:!state[obj.name]
+                }
+            ))
+        }
+        
+    }
+    function handleDeleteRow(e){
+        const i=e.target.getAttribute("name");
+        state.quantity.splice([i],1);
+        state.id.splice([i],1);
+        setState(p=>({
+            ...p,
+            exportItemCount:state.exportItemCount-1
+        }))
     }
     function handleOptionChange(e){
+        console.log(e.target.value);
         const obj=e.target;
+        const [itemId,i]=obj.value.split(" ");
         var item;
-        (state.currentItems.map(i=>{
-            if(i._id===obj.value){
+        (state.currentItems.map(j=>{
+            console.log(j._id,itemId);
+            if(j._id===itemId){
                 console.log("here");
-                item=i;
+                item=j;
                 return true;
             }   
-            return false;
+            return false;   
         }))
+        // state.id[]
+        // var quantity=state.quantity[i]=item.quantity;
+        state.id[[i]]=item._id;
+        state.quantity[[i]]=item.quantity
+        // state.quantity[[i]]=item.quantity
+        // var quantity=state.quantity;
+        // quantity[i]=item.quantity;
+        console.log(item,i);
         setState(p=>(
             {
                 ...p,
+                // quantity:quantity,
                 particular:item.name,
-                quantity:item.quantity
+                // quantity:item.quantity,
             }
         ));
 
     }
     useEffect(()=>{
+        if(state.showExportedItems){
         console.log(url);
-        // const url='http://localhost:3001/getitem'
         axios.get(url+'/getexported')
         .then(res=>{
             const items=res.data.data;
             setState(p=>{
                 return({
                     ...p,
-                    items:items
+                    exportedItems:items
                 });
             })
         })
         .catch(err=>console.log(err))
-    },[]);
+    }
+    },[state.showExportedItems]);
+     function exportItem(item,quantity){
+        var data={};
+        data.name=item.name
+        data.quantity=quantity
+        data.date=state.dateExported
+        data.expiry=item.expiry
+        data.description=item.description
+        data.receivedBy=item.receivedBy
+        data.receivedFrom=item.receivedFrom
+        data.exportedTo=state.exportedTo
+        console.log(data);
+        var p=new Promise(async(resolve,reject)=>{
+        await axios.post(url+'/exportitem',data)
+        .then(async res=>{
+            console.log(res);
+            if(item.quantity-quantity===0){
+                axios.delete(url+'/deleteitem/'+item._id)
+                .then(res=>{
+                    console.log(res);
+                    if(res===0){
+                        reject(0)
+                    }else
+                    resolve(1)
+                })
+                .catch((e)=>{
+                    reject(0);
+                })
+            }else{
+                var updateData={quantity:item.quantity-quantity}
+            await axios.put(url+'/update/'+item._id,updateData)
+            .then(res=>{
+                console.log(res);
+                resolve(1)
+            })
+            .catch((e)=>{
+                reject(0);
+            })
+            }
+            
+        })
+        .catch(err=>{
+            console.log(err);
+            reject(0);
+        })
+        })
+        return p;
+    }
+    function handleSubmit(){
+       setState(p=>(
+        {
+            ...p,
+            showLoader:true,
+            startExport:true
+        }
+       ))
+    }
+    function closeModalCallback(){
+        console.log("Callback");
+        setState(p=>({
+            ...p,
+            showModal:false,
+            message:"",
+            messageVariant:""}))
+    }
     return(
         <>
-        {state.particular}
+        <Modal message={state.message} type={state.messageVariant} closeCallback={closeModalCallback} show={state.showModal}/>
+        <Loader show={state.showLoader}/>
         <div style={{
             width:"100%"
         }}>
             <div>
-                <h2 style={{marginTop:"0",color:color.contrast}}>DASHBOARD </h2>
+                <h2 style={{marginTop:"0",color:color.contrast}}>EXPORTED ITEMS </h2>
             </div>
             <div style={{
                 margin:"auto",
                 marginTop:"4em"
             }}>
-                {!state.showExportForm&&
+                {state.showExportedItems&&
+                <>
                 <table style={{
                     margin:"auto",
                     width:"70%",
                     borderSpacing:"0"
-                }}>
+                    }}>
                     <tr>
                     <th className="th">
                             Date
@@ -111,7 +311,7 @@ function Export(){
                             Remarks
                         </th>
                     </tr>
-                    {state.items.map(i=>{
+                    {state.exportedItems.map(i=>{
                         return(
                           
                             <tr>
@@ -140,10 +340,28 @@ function Export(){
                         );
                     })}
                 </table>
+                <button onClick={handleClick} name="showExportForm1">
+                Export items
+                </button>
+                </>
                 }
-                {state.showExportForm&&
+                {state.showExportForm1&&
                 <>
-                    <table style={{
+                    {showPreForm()}   
+                </>}
+                {state.showExportForm2&&
+                <>
+                    {showFullForm()}   
+                </>}
+            </div>
+        </div>
+        </>
+    );
+function showFullForm(){
+    return(
+        
+        <>
+        <table style={{
                     margin:"auto",
                     width:"70%",
                     borderSpacing:"0"
@@ -154,46 +372,142 @@ function Export(){
                         </th>
                       
                         <th className="th">
-                            Quantiity
+                            Quantity
                         </th>
                         
                         <th className="th">
                             Expiry Date
                         </th>
                         <th className="th">
-                            Remarks
+                            Action
                         </th>
                     </tr>
-                    <tr >
+                    {[...Array(state.exportItemCount)].map((l,index)=>{
+                        // {console.log(index)}
+                        return(
+                            <>
+                            <tr >
                         <td className="td-1">
                             <select className="select" onChange={handleOptionChange} name="particular">
                                 <option style={{background:color.dark}} >Select</option>
                                 {state.currentItems.map((i,ind)=>{
                                     return(
-                                        <option style={{background:color.dark}} value={i._id}>{i.name}</option>
+                                        <option style={{background:color.dark}} value={i._id+" "+index}>{i.name}</option>
                                     );
                                 })}
                             </select>
                         </td>
                         <td className="td-1">
-                            <input className="inputitem" value={state.quantity} name="quantity" onChange={handleChange}/>
+                            <input className="inputitem" value={state.quantity[index]} name={index} onChange={handleQuantityChange}/>
                         </td>
                         <td className="td-1">
+                            hgbf
                         </td>
                         <td className="td-1">
+                        <button className="btn-add" style={{color:"grey"}}
+                                name={index}
+                                onClick={handleDeleteRow}    
+                            >
+                                X
+                                </button>
                         </td>
-                        <td className="td-1">
-                        </td>
-
                     </tr>
-                    </table>             
-                </>}
-                <button onClick={handleClick}>
-                    {state.showExportForm?"Cancel":"Export item"}
-                </button>
-            </div>
-        </div>
+                            </>
+                        );
+                        
+                    })}
+                    
+                    <tr>
+                    <td colSpan="1" className="td-add-btn">
+                            <button className="btn-add" onClick={handleClick} name="showExportForm1">Cancel</button>
+                        </td>
+                        <td colSpan="3" className="td-add-btn">
+                            <button className="btn-add"  onClick={handleAddItem} name="addItem">+</button>
+                        </td>
+                    </tr>
+                    </table>
+                    <button onClick={handleSubmit}>Export</button>
         </>
     );
+}
+function showPreForm(){
+    return(
+        <>
+        <table style={{
+                        margin:"auto",
+                        width:"70%",
+                        padding:"2em",
+                        borderSpacing:"0",
+                        borderRadius:"2px",
+                        background:"#2E333C",
+                        boxShadow:"1px 1px 2px 0px black"
+                    }}>
+                    <tr>
+                        <td colSpan="3" style={{textAlign:"right",paddingRight:"10px"}}>
+                            <button className="btn-add" style={{color:"grey"}}
+                                name="showExportedItems"
+                                onClick={handleClick}    
+                            >
+                                X
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                            <td className="tditem">
+                            EXPORT DATE
+                            </td>
+                            <td className="tditem">
+                                :
+                            </td>
+                            <td className="tditem">
+                            <input
+                            className="inputitem"
+                            name="dateExported"
+                            value={state.dateExported}
+                            onChange={handleChange}
+                            readOnly/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="tditem">
+                            EXPORT TO
+                            </td>
+                            <td className="tditem">
+                                :
+                            </td>
+                            <td className="tditem">
+                            <input
+                            className="inputitem"
+                            name="exportedTo"
+                            value={state.exportedTo}
+                            onChange={handleChange}
+                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="tditem">
+                            PURPOSE
+                            </td>
+                            <td className="tditem">
+                                :
+                            </td>
+                            <td className="tditem">
+                            <input
+                            className="inputitem"
+                            name="purpose"
+                            value={state.purpose}
+                            onChange={handleChange}
+                            />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3" className="td-add-btn">
+                                <button className="btn-add" onClick={handleClick} name="showExportForm2">Proceed</button>
+                            </td>
+                        </tr>
+            </table>
+        </>
+    );
+}
 }   
 export default Export;
